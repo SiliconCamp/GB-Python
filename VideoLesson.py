f978028c4a4a774820f224354a6ba4b343ca0cc5
SiliconCamp <camp@silicon-camp.ru>
2018-09-07 18:14:37 +0300

    Added "while" cycle to do not exit until "Q"

--- VideoLesson.py
+++ VideoLesson.py
@@ -12,36 +12,38 @@ print("Привет, программист!")
 name = input("Ваше имя: ")
 print(name, ", добро пожаловать в мир Python!")
 
-answer = input("Давайте поработаем? (Y/N)")
-
 #PEP-8
-if answer == "Y":
-    print("[1] - выведу список файлов")
-    print("[2] - выведу информацию о системе")
-    print("[3] - выведу список процессов")
-    print("[4]")
-    print("[5]")
-    print("[6]")
-    print("[7]")
-    print("[8]")
-    print("[9]")
-    act = int(input("Выберите действие(1-9): "))
+answer = ""
 
-    if act == 1:
-        print(os.listdir())
-    elif act == 2:
-        print("Вот что я знаю о системе:")
-        print("Количество процессоров: ", psutil.cpu_count())
-        print("Платформа: ", sys.platform)
-        print("Кодировка файловой системы: ", sys.getfilesystemencoding())
-        print("Текущая директория: ", os.getcwd())
-        print("Текущий пользователь: ", os.getlogin())
-    elif act == 3:
-        print(psutil.pids())
+while answer != "Q":
+    answer = input("Давайте поработаем? (Y/N/Q)")
+    if answer == "Y":
+        print("[1] - выведу список файлов")
+        print("[2] - выведу информацию о системе")
+        print("[3] - выведу список процессов")
+        print("[4]")
+        print("[5]")
+        print("[6]")
+        print("[7]")
+        print("[8]")
+        print("[9]")
+        act = int(input("Выберите действие(1-9): "))
+
+        if act == 1:
+            print(os.listdir())
+        elif act == 2:
+            print("Вот что я знаю о системе:")
+            print("Количество процессоров: ", psutil.cpu_count())
+            print("Платформа: ", sys.platform)
+            print("Кодировка файловой системы: ", sys.getfilesystemencoding())
+            print("Текущая директория: ", os.getcwd())
+            print("Текущий пользователь: ", os.getlogin())
+        elif act == 3:
+            print(psutil.pids())
+        else:
+            pass
+
+    elif answer == "N":
+        print("До свидания!")
     else:
-        pass
-
-elif answer == "N":
-    print("До свидания!")
-else:
-    print("Неизвестный выбор")
+        print("Неизвестный выбор")
